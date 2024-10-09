75070eaf08b6d3280b6a08117b55b5f7ae0fce64
Abdirizaq5 <n3heab00@students.oamk.fi>
2024-10-09 09:01:19 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>REACT</h1>
+        <ul>
+          <li>Component</li>
+          <li>Events</li>
+          <li>State management</li>
+        </ul>
+    </div>
+      
+  )
+}
+
+export default App
